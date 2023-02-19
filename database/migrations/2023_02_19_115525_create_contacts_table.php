912483c1bd56ab250c2b773bef2c1b582b4e5fd2
Fanuel Ramos <fanuelramos111@gmail.com>
2023-02-19 15:28:00 +0100

    refactor: add SoftDelete

--- database/migrations/2023_02_19_115525_create_contacts_table.php
+++ database/migrations/2023_02_19_115525_create_contacts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('contact')->unique();
             $table->string('email')->unique();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
